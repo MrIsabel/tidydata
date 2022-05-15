102e7db77e2744327de6b2ea431fec30a64ba1aa
MrIsabel <marc.isabel.2011@gmail.com>
2022-05-15 17:02:00 -0400

    First Tidy Set Achieved

--- tidyset.R
+++ tidyset.R
@@ -1,4 +1,14 @@
+  # The file "getdata_projectfiles_UCI HAR Dataset.zip" needs to be unzipped in
+  # the directory where the function tidyset.R is.
+
 tidyset <- function(){
+  
+  # Check if dplyr package is installed.
+  
+  if(!require(dplyr)){
+    stop("dplyr not installed")
+  }
+  else{
   
   pathfile1 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt"
   pathfile2 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt"
@@ -7,7 +17,8 @@ tidyset <- function(){
   pathfile5 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt"
   pathfile6 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt"
   pathfile7 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt"
-  pathfile8 <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt"
+  
+  # Read the different text files.
   
   test_measure <- read.table(pathfile1, header= FALSE)
   test_activity <- read.table(pathfile2, header= FALSE)
@@ -16,18 +27,44 @@ tidyset <- function(){
   train_activity <- read.table(pathfile5, header= FALSE)
   train_subject <- read.table(pathfile6, header= FALSE)
   measurement <- read.table(pathfile7, header = FALSE)
-  activity_name <- read.table(pathfile8, header = FALSE)
   
-  testmerged <- cbind(test_subject, test_activity, test_measure)
-  trainmerged <- cbind(train_subject, train_activity, train_measure)
-  completedata <- rbind(testmerged, trainmerged)
+  # Merge all the data frames into one.
   
-  collumn_label <- c("subject_id", "activity_id", measurement[,2])
-  colnames(completedata) <- collumn_label
+  test_merged <- cbind(test_subject, test_activity, test_measure)
+  train_merged <- cbind(train_subject, train_activity, train_measure)
+  complete_data <- rbind(test_merged, train_merged)
   
-  str(completedata)
+  # Name all variables.
+  
+  column_label <- c("subject_id", "activity_name", measurement[,2])
+  colnames(complete_data) <- column_label
+  
+  # Create a new data frame by keeping the first two rows and all the variables 
+  # with mean() et std() in their name.
+  
+  pattern <- c("mean\\(\\)", "std\\(\\)", "subject_id", "activity_name" )
+  selected_data <- complete_data[,grepl(paste(pattern,collapse="|" ), names(complete_data))]
+  colnames(selected_data) <- tolower(gsub("\\(", "", 
+                                     gsub("\\)", "",
+                                     gsub("-","_", colnames(selected_data)))
+                                     ))
+  
+  # Replace activity codes with activity names.
+  
+  selected_data <- selected_data %>% mutate(activity_name = 
+                        case_when(activity_name == 1 ~ "walking",
+                                  activity_name == 2 ~ "walking upstairs",
+                                  activity_name == 3 ~ "walking downstairs",
+                                  activity_name == 4 ~ "sitting",
+                                  activity_name == 5 ~ "standing",
+                                  activity_name == 6 ~ "laying"
+                                  ))
+  
+  
+  str(selected_data)
+  # selected.data
+  
+  }
 
-  
-  
-  
+
 }
